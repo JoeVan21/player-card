19ef9108bfe5f2053253f2ae3079de691140fa2c
JoeVan21 <jmv5650@psu.edu>
2023-02-16 13:01:09 -0500

    Update player-card.js

--- src/player-card.js
+++ src/player-card.js
@@ -8,46 +8,49 @@ class PlayerCard extends LitElement {
   };
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--player-card-background-color);
+  img{
+    max-width: 400px;
+  }
+  button {
+    cursor:pointer;
+    margin: 1px;
+    border: 2px dashed blue;
+    color:#00FFFF; 
+    background-color: black;
+    border-radius:20px;
+   text-transform: uppercase;
+  padding: 8px 8px;
+  }
+  .card {
+    font-size: 15px;
+    margin: 5px;
+    padding: 10px;
+    text-align:center;
+    border: solid blue 10px;
+    padding: 8px;
+    max-width: 400px;
+  }
+  .borders {
+   justify-content:center;
+    display:flex;
+  }
+  button:hover{
+    background-color: #0400FE;
+  }
+  
+  button:focus{
+    background-color: #0400FE;
+  }
+  
+  
+  @media screen and (min-width:501px) and (max-width:800px) {
+  
+  }
+  @media screen and (max-width:500px) {
+    .card{
+      font-size: 3vw
     }
-
-    main {
-      flex-grow: 1;
-    }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
+  }
   `;
 
   constructor() {
@@ -57,30 +60,27 @@ class PlayerCard extends LitElement {
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/PlayerCard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
+    <div class="buttons">
+  
+    <button class="btnDup">Duplicate</button>
+    <button id="del">Delete</button>
+    <button class="btnDet">Details</button> 
+    <button class="btnHead">Change Header</button>
+    <button  class="btnBack">Change Background</button>
+    
+    </div>
+  
+  <div class="borders">
+    <div class="card">
+    <h2>Nick Singleton </h2> 
+  
+  <img src=https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTkyMjQwNTI1ODcxNDI1MDcw/nicholas-singleton.jpg alt="Nick Singleton" /> 
+    
+    <div>
+      <p class="body" >Star Football Runningback for the Penn State Nittany Lions!<br>Big Ten's Freshman of the Year in 2022</p>
+      </div>
+    </div>
+  </div>
     `;
   }
 }
